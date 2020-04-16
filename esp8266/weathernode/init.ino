2ba18ba62c7784d24fc0326ad24c7827c5657bd8
Matthias Uschok <dev@uschok.de>
2020-04-16 23:12:47 +0200

    Increased oversampling and filtering for ESP8266

--- esp8266/weathernode/init.ino
+++ esp8266/weathernode/init.ino
@@ -35,10 +35,10 @@ void start_i2c() {
 
     bme.setSampling(
         Adafruit_BME280::MODE_FORCED,
-        Adafruit_BME280::SAMPLING_X2, // temperature
-        Adafruit_BME280::SAMPLING_X4, // pressure
-        Adafruit_BME280::SAMPLING_X2, // humidity
-        Adafruit_BME280::FILTER_X2
+        Adafruit_BME280::SAMPLING_X8, // temperature
+        Adafruit_BME280::SAMPLING_X8, // pressure
+        Adafruit_BME280::SAMPLING_X8, // humidity
+        Adafruit_BME280::FILTER_X4
     );
 }
 
